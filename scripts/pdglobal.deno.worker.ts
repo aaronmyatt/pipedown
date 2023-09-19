5897b03962097bc00b60e10a1dd88f2d61cd16ac
Aaron Myatt <aaronmyatt@gmail.com>
2023-09-19 17:22:58 +0800

    call pipe with intialInputs

--- scripts/pdglobal.deno.worker.ts
+++ scripts/pdglobal.deno.worker.ts
@@ -1,19 +1,19 @@
-import utils, {savePipeInput} from "../utils.ts";
+import utils from "../utils.ts";
 import {executeScript} from './executeScript.deno.inject.ts'
 
 const PD = {}
 
 self.PD = new Proxy(PD, {
-    get(target, prop, receiver) {
+    get(target, prop, _receiver) {
         if (prop in target) {
             return (pipeopts) => Promise.resolve(target[prop](Object.assign({
-                always: (state, input) => {
+                always: (_state, _input) => {
                 }
             }, pipeopts)))
         } else {
             const DEFAULT_OPTS = {}
             return async (pipeopts = {}) => {
-                const inputs = Object.fromEntries(Object.entries(pipeopts).filter(([key, value]) => !Object.keys(DEFAULT_OPTS).includes(key)));
+                const inputs = Object.fromEntries(Object.entries(pipeopts).filter(([key, _value]) => !Object.keys(DEFAULT_OPTS).includes(key)));
                 if(prop === 'temp'){
                     return executeScript({name: utils.pipeScriptName({id: 'temp'})}, inputs)
                 }
