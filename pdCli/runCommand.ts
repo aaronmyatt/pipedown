269ca75449631e43b7235991ac44a0a2d2a57a4e
Aaron Myatt <aaronmyatt@gmail.com>
2024-03-01 08:45:32 +0800

    wraps command with start and end events

--- pdCli/runCommand.ts
+++ pdCli/runCommand.ts
@@ -9,6 +9,19 @@ const inputParam = $p.compile('/flags/input')
 export async function runCommand(input: pdCliInput) {
   const command = commandName.get(input);
   const testInput = inputRaw.get(input) || inputParam.get(input) || "{}";
-  await pdRun(command, JSON.stringify(testInput));
+
+  dispatchRunStartEvent(input);
+  await pdRun(command, JSON.stringify(testInput))
+  dispatchRunEndEvent(input);
   return input;
+}
+
+function dispatchRunStartEvent(input: pdCliInput){
+  const event = new CustomEvent('pdrunstart', {detail: input})
+  dispatchEvent(event)
+}
+
+function dispatchRunEndEvent(input: pdCliInput){
+  const event = new CustomEvent('pdrunend', {detail: input})
+  dispatchEvent(event)
 }
