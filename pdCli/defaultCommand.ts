c44d7a13f7c4b9a1117bd51345ef62f622988eb9
Aaron Myatt <aaronmyatt@gmail.com>
2024-03-06 16:04:43 +0800

    move listener streams into defaultcommand

--- pdCli/defaultCommand.ts
+++ pdCli/defaultCommand.ts
@@ -5,6 +5,41 @@ import { reportErrors } from "./reportErrors.ts";
 import { basename } from "https://deno.land/std@0.206.0/path/basename.ts";
 import { colors } from "./helpers.ts";
 import { testCommand } from "./testCommand.ts";
+import {firstNotNullishOf} from "https://deno.land/std@0.208.0/collections/first_not_nullish_of.ts";
+import {parse as keycodeParse} from "https://deno.land/x/cliffy@v1.0.0-rc.3/keycode/key_code.ts";
+
+const listenForKeypresses = async () => {
+  addEventListener("keypress", async (e) => {
+    const detail = (e as CustomEvent).detail;
+    if (detail.keycode.name === "c" && detail.keycode.ctrl) {
+      if(globalThis.processes && globalThis.processes.length > 0){ // @ts-expect-error helper to close any running process
+        for (const process of globalThis.processes) {
+          try {
+            process.kill();
+          } catch (e) {
+            console.error(e)
+          }
+        }
+      }
+      console.log('Exiting')
+      Deno.exit();
+    }
+
+    if (detail.keycode.name === "e") {
+      console.log('Exporting')
+    }
+    console.log(detail);
+  });
+
+  // Deno.stdin.setRaw(true)
+  Deno.stdin.setRaw(true, {cbreak: true})
+  for await (const stdin of Deno.stdin.readable) {
+    const keycode = firstNotNullishOf(keycodeParse(stdin), (k => k))
+    if (keycode) {
+      dispatchEvent(new CustomEvent('keypress', {detail: {keycode}}))
+    }
+  }
+}
 
 export async function defaultCommand(input: pdCliInput) {
   console.log(colors.brightGreen("Watching for changes..."));
@@ -12,7 +47,6 @@ export async function defaultCommand(input: pdCliInput) {
 
   const lazyIO = debounce(async (input: pdCliInput) => {
     Object.assign(input, await pdBuild(input));
-    Object.assign(input, await testCommand(input));
     if (input.errors && input.errors.length > 0) {
       reportErrors(input);
     }
@@ -37,18 +71,23 @@ export async function defaultCommand(input: pdCliInput) {
     "e: export",
   ].join(" | "));
 
-  for await (const event of Deno.watchFs(".", { recursive: true })) {
+  for await (const event of Deno.watchFs(".", { recursive: true,  })) {
     const notInProtectedDir = event.paths.every((path) =>
       !path.match("\.pd|deno|dist")
     );
 
+    const extensions = [".md"];
+    const hasValidExtension = event.paths.every((path) =>
+      extensions.some((ext) => path.endsWith(ext))
+    );
+
     if (
-      event.kind === "modify" && event.paths.length === 1 && notInProtectedDir
+      event.kind === "modify" && event.paths.length === 1 && notInProtectedDir && hasValidExtension
     ) {
       const fileName = basename(event.paths[0]);
       console.log(colors.brightGreen(`File changed: ${fileName}`));
       lazyIO({match: fileName, ...input});
-      lazyDispatchFileChanged(input);
+      //lazyDispatchFileChanged(input);
     }
   }
 
