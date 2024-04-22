5d1bcc397dc850e7b465a8d260426742da004c30
Aaron Myatt <aaronmyatt@gmail.com>
2024-04-22 13:55:11 +0800

    remove before and after events

--- pdPipe.ts
+++ pdPipe.ts
@@ -3,16 +3,6 @@ import Pipeline from "./pipeline.ts";
 import { funcWrapper } from "./pdUtils.ts";
 
 export default function Pipe<I extends Input>(funcs: Stage<I>[], opts: Pipe): Pipeline<I> {
-  const dispatchOnStart = (input: I) => {
-    const event = new CustomEvent("pd::pipe::start", { detail: input });
-    globalThis.dispatchEvent(event);
-  }
-  funcs.push((input: I) => {
-    // dispatch custom event at end of pipeline
-    const event = new CustomEvent("pd::pipe::processed", { detail: input });
-    globalThis.dispatchEvent(event);
-  })
-
   const wrappedFuncs = funcWrapper(funcs, opts)
   return new Pipeline(wrappedFuncs);
 }
