a9984fc8eca45346d69cde045d88ba4a02719f2a
Aaron Myatt <aaronmyatt@gmail.com>
2024-05-31 00:19:23 +0800

    running as cli is optionally noisy

--- stringTemplates.ts
+++ stringTemplates.ts
@@ -99,19 +99,24 @@ ${
 export const pdCliTemplate = () =>
   `import pipe from "./index.ts"
 import {parseArgs} from "jsr:@std/cli@0.224.0";
+import $p from "jsr:@pd/pointers@0.1.1";
 
 const flags = parseArgs(Deno.args);
-const output = await pipe.process({ flags, mode: { cli: true } })
+const input = JSON.parse(flags.input || flags.i || '{}');
+$p.set(input, "/flags", flags);
+$p.set(input, "/mode/cli", true);
+
+const output = await pipe.process(input)
 if(output.errors){
   console.error(output.errors)
   Deno.exit(1);
 }
 if(flags.pretty || flags.p){
   console.log(output);
-} else {
+} else if(flags.json || flags.j) {
   console.log(JSON.stringify(output));
-  Deno.exit(0);
 }
+Deno.exit(0);
 `;
 
 export const pdServerTemplate = () =>
