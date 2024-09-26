7ac5f86a724b419f84be8d9b468bf7c66d897741
Aaron Myatt <aaronmyatt@gmail.com>
2024-09-26 23:22:53 +0800

    reintroduce serve command

--- pdCli/mod.ts
+++ pdCli/mod.ts
@@ -1,33 +1,33 @@
-import type { PDError, Input, PipeConfig } from "../pipedown.d.ts";
+import type { Input, PDError, PipeConfig } from "../pipedown.d.ts";
 import type { Args } from "jsr:@std/cli@1.0.6";
 import type { WalkEntry } from "jsr:@std/fs@1.0.3/walk";
 import type { ParsedPath } from "jsr:@std/path@1.0.4/parse";
 import projectMetadata from "./../deno.json" with { type: "json" };
 
-import {
-    PD_DIR,
-    pdRun,
-} from "./helpers.ts";
+import { PD_DIR, pdRun } from "./helpers.ts";
 
-import {std, pd} from "../deps.ts";
+import { pd, std } from "../deps.ts";
 
-const {process} = pd;
+const { process } = pd;
 const version = projectMetadata.version;
 
-import {helpCommand} from "./helpCommand.ts";
-import {buildCommand} from "./buildCommand.ts";
-import {runCommand} from "./runCommand.ts";
-import {runWithCommand} from "./runWithCommand.ts";
-import {listCommand} from "./listCommand.ts";
-import {testCommand} from "./testCommand.ts";
-import {cleanCommand} from "./cleanCommand.ts";
-import {defaultCommand} from "./defaultCommand.ts";
-import {helpText} from "../stringTemplates.ts";
+import { helpCommand } from "./helpCommand.ts";
+import { buildCommand } from "./buildCommand.ts";
+import { runCommand } from "./runCommand.ts";
+import { runWithCommand } from "./runWithCommand.ts";
+import { serveCommand } from "./serveCommand.ts";
+import { listCommand } from "./listCommand.ts";
+import { testCommand, updateTestCommand } from "./testCommand.ts";
+import { cleanCommand } from "./cleanCommand.ts";
+import { defaultCommand } from "./defaultCommand.ts";
+import { helpText } from "../stringTemplates.ts";
 
 async function pdInit(input: pdCliInput) {
     try {
         await Deno.mkdir(PD_DIR);
-        console.log(std.colors.brightCyan("First time here? Welcome to Pipe ↓!"));
+        console.log(
+            std.colors.brightCyan("First time here? Welcome to Pipe ↓!"),
+        );
         console.log(std.colors.brightGreen("Creating ~/.pd"));
     } catch (e) {
         if (e.name !== "AlreadyExists") throw e;
@@ -35,12 +35,12 @@ async function pdInit(input: pdCliInput) {
 
     // read global config file, config.json, from the current directory,
     // if it exists
-    const configPath = std.join(Deno.cwd(), 'config.json');
+    const configPath = std.join(Deno.cwd(), "config.json");
     input.globalConfig = {
         on: {},
         emit: true,
         persist: true,
-        ...input.globalConfig
+        ...input.globalConfig,
     };
     try {
         const config = JSON.parse(await Deno.readTextFile(configPath));
@@ -53,18 +53,18 @@ async function pdInit(input: pdCliInput) {
 async function registerProject(input: pdCliInput) {
     const project = {
         name: input.globalConfig.name || std.parsePath(Deno.cwd()).name,
-        path: Deno.cwd()
+        path: Deno.cwd(),
     };
 
     const home = Deno.env.get("HOME");
-    if(home){
+    if (home) {
         try {
-            await Deno.mkdir(std.join(home, '.pipedown'), { recursive: true });
+            await Deno.mkdir(std.join(home, ".pipedown"), { recursive: true });
         } catch (e) {
             if (e.name !== "AlreadyExists") throw e;
         }
 
-        const projectsPath = std.join(home, '.pipedown', "projects.json");
+        const projectsPath = std.join(home, ".pipedown", "projects.json");
         let projects = [];
         try {
             projects = JSON.parse(projectsPath);
@@ -72,13 +72,22 @@ async function registerProject(input: pdCliInput) {
             // probably the first project
         }
         projects.push(project);
-        await Deno.writeTextFile(projectsPath, JSON.stringify(projects, null, 2), { create: true });
+        await Deno.writeTextFile(
+            projectsPath,
+            JSON.stringify(projects, null, 2),
+            { create: true },
+        );
     }
 }
 
-export function checkFlags(flags: string[], func: (input: pdCliInput) => Promise<pdCliInput> | pdCliInput): (input: pdCliInput) => Promise<pdCliInput> | pdCliInput {
+export function checkFlags(
+    flags: string[],
+    func: (input: pdCliInput) => Promise<pdCliInput> | pdCliInput,
+): (input: pdCliInput) => Promise<pdCliInput> | pdCliInput {
     return (input: pdCliInput) => {
-        if (input.flags._.length === 0 && flags[0] === "none") return func(input);
+        if (input.flags._.length === 0 && flags[0] === "none") {
+            return func(input);
+        }
 
         const flagsMatch = input.flags._.length &&
             input.flags._.every((flag, index) => {
@@ -91,46 +100,49 @@ export function checkFlags(flags: string[], func: (input: pdCliInput) => Promise
 
 const gatherProjectContext = async (input: pdCliInput) => {
     input.projectPipes = [];
-    const opts = {exts: [".md"], skip: [/node_modules/, /\.pd/]};
+    const opts = { exts: [".md"], skip: [/node_modules/, /\.pd/] };
     for await (const entry of std.walk(".", opts)) {
         input.projectPipes.push({
             path: entry.path,
             entry,
-            ...std.parsePath(entry.path)
+            ...std.parsePath(entry.path),
         });
     }
     return input;
-}
+};
 
 const startListeners = (input: pdCliInput) => {
     input.globalConfig.on = input.globalConfig.on || {};
     for (const key in input.globalConfig.on) {
         const scripts = input.globalConfig.on[key];
         if (!Array.isArray(scripts)) {
-            throw new Error(`Expected an array of scripts for the config key: on.${key}`);
+            throw new Error(
+                `Expected an array of scripts for the config key: on.${key}`,
+            );
         }
 
         addEventListener(key, async (_e) => {
             //console.log(`Running scripts for event: ${key}`);
-            await Promise.all(scripts.map(async (script: (string | {[p: string]: Input})) => {
-                console.log(`Running script: ${script}`);
-                if(typeof script === "string") {
-                    await pdRun(script, "{}");
-                }
-                else {
-                    for (const key in script) {
-                        await pdRun(key, JSON.stringify(script[key]));
+            await Promise.all(
+                scripts.map(async (script: string | { [p: string]: Input }) => {
+                    console.log(`Running script: ${script}`);
+                    if (typeof script === "string") {
+                        await pdRun(script, "{}");
+                    } else {
+                        for (const key in script) {
+                            await pdRun(key, JSON.stringify(script[key]));
+                        }
                     }
-                }
-            }));
+                }),
+            );
         });
     }
-}
+};
 
 const versionCommand = (input: pdCliInput) => {
     console.log(version);
     return input;
-}
+};
 
 const funcs = [
     pdInit,
@@ -144,33 +156,54 @@ const funcs = [
     checkFlags(["clean"], cleanCommand),
 
     checkFlags(["build"], buildCommand),
+    checkFlags(["serve", "*", "*"], serveCommand),
     checkFlags(["run", "*", "*"], runCommand),
     checkFlags(["runWith", "*", "*", "*"], runWithCommand),
     checkFlags(["test"], testCommand),
-    checkFlags(["version"], versionCommand)
+    checkFlags(["test-update"], updateTestCommand),
+    checkFlags(["t"], testCommand),
+    checkFlags(["tu"], updateTestCommand),
+    checkFlags(["version"], versionCommand),
 ];
 
-const debugParamPresent = Deno.env.get("DEBUG") || Deno.args.includes("--debug") ||
+const debugParamPresent = Deno.env.get("DEBUG") ||
+    Deno.args.includes("--debug") ||
     Deno.args.includes("-d") || Deno.args.includes("--DEBUG") ||
     Deno.args.includes("-D");
 export interface pdCliInput extends Input {
-    flags: Args,
-    globalConfig: PipeConfig,
-    projectPipes: Array<{ path: string, entry: WalkEntry } & ParsedPath>,
-    errors?: Array<PDError>,
-    output: Input,
-    debug: boolean | string,
-    match?: string,
+    flags: Args;
+    globalConfig: PipeConfig;
+    projectPipes: Array<{ path: string; entry: WalkEntry } & ParsedPath>;
+    errors?: Array<PDError>;
+    output: Input;
+    debug: boolean | string;
+    match?: string;
 }
 
 // @ts-ignore - this is a Deno specific API
-const flags: Args = std.parseArgs(Deno.args, {"--": true, boolean: ["json", "pretty", "j", "p", "debug", "d", "DEBUG", "D", "version", "v", "help", "h"]});
-if(flags.version || flags.v) {
+const flags: Args = std.parseArgs(Deno.args, {
+    "--": true,
+    boolean: [
+        "json",
+        "pretty",
+        "j",
+        "p",
+        "debug",
+        "d",
+        "DEBUG",
+        "D",
+        "version",
+        "v",
+        "help",
+        "h",
+    ],
+});
+if (flags.version || flags.v) {
     console.log(version);
     Deno.exit(0);
 }
 
-if(flags.help || flags.h) {
+if (flags.help || flags.h) {
     console.log(helpText);
     Deno.exit(0);
 }
@@ -180,27 +213,26 @@ const output = await process<pdCliInput>(funcs, {
     globalConfig: {} as PipeConfig,
     projectPipes: [],
     errors: [],
-    output: {errors: []} as Input,
+    output: { errors: [] } as Input,
     debug: debugParamPresent,
 }, {});
 
 if (output.errors && output.errors.length > 0) {
     output.errors.forEach((error: PDError) => {
-        console.log(error.name)
-        console.log('function: ', error.func)
-        console.log(error.message)
-        console.log(error.stack)
-        console.log('---')
-    })
+        console.log(error.name);
+        console.log("function: ", error.func);
+        console.log(error.message);
+        console.log(error.stack);
+        console.log("---");
+    });
     Deno.exit(1);
 } else {
     if (output.debug) {
         console.log(output);
-    } else if(flags.json || flags.j) {
+    } else if (flags.json || flags.j) {
         console.log(JSON.stringify(output.output, null, 2));
     } else if (flags.pretty || flags.p) {
         console.log(output.output);
     }
     Deno.exit(0);
 }
-
---
+++ pdCli/serveCommand.ts
@@ -0,0 +1,37 @@
+import type { pdCliInput } from "./mod.ts";
+import { pdServe } from "./helpers.ts";
+import { pd } from "../deps.ts";
+import { pdBuild } from "../pdBuild.ts";
+import { cliHelpTemplate } from "../stringTemplates.ts";
+
+const commandName = pd.$p.compile("/flags/_/1");
+const inputRaw = pd.$p.compile("/flags/_/2");
+const inputParam = pd.$p.compile("/flags/input");
+
+const helpText = cliHelpTemplate({
+  title: "Serve",
+  command: "pd serve [options] [file]",
+  sections: [
+    "Run a pipe as a server.",
+    `Example:
+    pd serve file.md`,
+    `Options:
+    -j, --json    Output the build information as JSON.
+    -p, --pretty  Pretty print the JSON output.
+    -h, --help    Display this message.
+    -d, --debug   Display debug information.
+    --input       Initial input for the pipedown file. Needs to be a JSON string.`,
+  ],
+});
+
+export async function serveCommand(input: pdCliInput) {
+  if (pd.$p.get(input, "/flags/help") || pd.$p.get(input, "/flags/h")) {
+    console.log(helpText);
+  } else {
+    const command = commandName.get(input);
+    const testInput = inputRaw.get(input) || inputParam.get(input) || "{}";
+    await pdBuild(input);
+    await pdServe(command, testInput);
+  }
+  return input;
+}
